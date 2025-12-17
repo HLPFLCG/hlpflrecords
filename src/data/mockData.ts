1c6a1bc57a614b35ba378e39e563f76a15da0577
HLPFL <admin@hlpfl.co>
2025-12-17 02:20:59 -0500

    Update mock data for releases and news posts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -18,17 +18,17 @@ export const mockArtists: Artist[] = [
 export const mockReleases: Release[] = [
   {
     id: '1',
-    title: 'Debut Single',
-    artist: 'Artist Name',
+    title: 'New Single',
+    artist: 'Alki',
     artistId: '1',
     type: 'single',
-    releaseDate: new Date('2024-01-15'),
-    coverArt: '/images/releases/placeholder.jpg',
-    description: 'Amazing debut single from our newest artist.',
+    releaseDate: new Date('November 28, 2025'),
+    coverArt: 'public/images/team/IMG_2768.png',
+    description: 'Latest Release from Alki',
     links: {
-      spotify: '#',
-      apple: '#',
-      youtube: '#',
+      spotify: 'https://open.spotify.com/album/468FaHLWX75g0RJdoEUABD?si=PwvPVHpiSIC9TJ5irgh3zg',
+      apple: 'https://api.ffm.to/sl/e/c/221?cd=eyJ1YSI6eyJ1YSI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzYwNS4xLjE1IChLSFRNTCwgbGlrZSBHZWNrbykgVmVyc2lvbi8yNi4xIFNhZmFyaS82MDUuMS4xNSIsImJyb3dzZXIiOnsibmFtZSI6IlNhZmFyaSIsInZlcnNpb24iOiIyNi4xIiwibWFqb3IiOiIyNiJ9LCJjcHUiOnt9LCJkZXZpY2UiOnsibW9kZWwiOiJNYWNpbnRvc2giLCJ2ZW5kb3IiOiJBcHBsZSJ9LCJlbmdpbmUiOnsibmFtZSI6IldlYktpdCIsInZlcnNpb24iOiI2MDUuMS4xNSJ9LCJvcyI6eyJuYW1lIjoibWFjT1MiLCJ2ZXJzaW9uIjoiMTAuMTUuNyJ9fSwiY2xpZW50Ijp7InJpZCI6IjczZDZhOTc0LWUwYTctNGNhNy1hZmI4LWZiYWMxZTk1OGFhYiIsInNpZCI6IjY3ZDEwYThjLWE4ZDUtNDIwMS04NDI3LTBkMTk2ZjAxNTI0NSIsImlwIjoiMjE3LjIxNy4xMTMuMTYxIiwicmVmIjoiaHR0cHM6Ly9sLmluc3RhZ3JhbS5jb20vIiwiaG9zdCI6InRvby5mbSIsImxhbmciOiJlbi1VUyIsImlwQ291bnRyeSI6IlVTIn0sImlzV2VicFN1cHBvcnRlZCI6ZmFsc2UsImdkcHJFbmZvcmNlIjpmYWxzZSwiY291bnRyeUNvZGUiOiJVUyIsImlzQm90IjpmYWxzZSwidXNlQWZmIjoib3JpZ2luIiwidmlkIjoiZjJmNWZmZjItNGM3Zi00MDkwLTk3MGItNGNhZDE3M2ZlNmFjIiwiaWQiOiI2OTE1MDNlZjM4MDAwMDNlMDA0MGYyZmEiLCJwcnYiOmZhbHNlLCJpc1ByZVIiOmZhbHNlLCJ0em8iOm51bGwsImNoIjpudWxsLCJhbiI6bnVsbCwiZGVzdFVybCI6Imh0dHBzOi8vZ2VvLm11c2ljLmFwcGxlLmNvbS91cy9hbGJ1bS8yMjEtc2luZ2xlLzE4NTMzNjE3NzI_YXBwPW11c2ljJmxzPTEmY3Q9RkZNXzhiNjVhN2M1MjIwMjdhNjRhYTA3YzFiMTcxYTAzZjM0JmF0PTEwMTBsMzgyOCIsInNydmMiOiJhcHBsZSIsInByb2R1Y3QiOiJzbWFydGxpbmsiLCJzaG9ydElkIjoiMjIxIiwiaXNBdXRob3JpemF0aW9uUmVxdWlyZWQiOmZhbHNlLCJvd25lciI6IjVlZTZiY2ViMzIwMDAwNjQyMmRkM2E2OCIsImFyIjoiNjE4MTJkYWYyNTAwMDBlNjc3YWZlN2YyIiwiaXNTaG9ydExpbmsiOmZhbHNlLCJuYXRpdmUiOmZhbHNlfQ',
+      youtube: 'https://api.ffm.to/sl/e/c/221?cd=eyJ1YSI6eyJ1YSI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzYwNS4xLjE1IChLSFRNTCwgbGlrZSBHZWNrbykgVmVyc2lvbi8yNi4xIFNhZmFyaS82MDUuMS4xNSIsImJyb3dzZXIiOnsibmFtZSI6IlNhZmFyaSIsInZlcnNpb24iOiIyNi4xIiwibWFqb3IiOiIyNiJ9LCJjcHUiOnt9LCJkZXZpY2UiOnsibW9kZWwiOiJNYWNpbnRvc2giLCJ2ZW5kb3IiOiJBcHBsZSJ9LCJlbmdpbmUiOnsibmFtZSI6IldlYktpdCIsInZlcnNpb24iOiI2MDUuMS4xNSJ9LCJvcyI6eyJuYW1lIjoibWFjT1MiLCJ2ZXJzaW9uIjoiMTAuMTUuNyJ9fSwiY2xpZW50Ijp7InJpZCI6IjczZDZhOTc0LWUwYTctNGNhNy1hZmI4LWZiYWMxZTk1OGFhYiIsInNpZCI6IjY3ZDEwYThjLWE4ZDUtNDIwMS04NDI3LTBkMTk2ZjAxNTI0NSIsImlwIjoiMjE3LjIxNy4xMTMuMTYxIiwicmVmIjoiaHR0cHM6Ly9sLmluc3RhZ3JhbS5jb20vIiwiaG9zdCI6InRvby5mbSIsImxhbmciOiJlbi1VUyIsImlwQ291bnRyeSI6IlVTIn0sImlzV2VicFN1cHBvcnRlZCI6ZmFsc2UsImdkcHJFbmZvcmNlIjpmYWxzZSwiY291bnRyeUNvZGUiOiJVUyIsImlzQm90IjpmYWxzZSwidXNlQWZmIjoib3JpZ2luIiwidmlkIjoiZjJmNWZmZjItNGM3Zi00MDkwLTk3MGItNGNhZDE3M2ZlNmFjIiwiaWQiOiI2OTE1MDNlZjM4MDAwMDNlMDA0MGYyZmEiLCJwcnYiOmZhbHNlLCJpc1ByZVIiOmZhbHNlLCJ0em8iOm51bGwsImNoIjpudWxsLCJhbiI6bnVsbCwiZGVzdFVybCI6Imh0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3BsYXlsaXN0P2xpc3Q9T0xBSzV1eV9sdGMtR3ZScXdrMnBOdmN0RVoySlQtVGRQTE5xYmFOajAmc3JjPUZGTSZsaWQ9MDAwMDAwMDAtNjkxNS0wM2VmLTM4MDAtMDAzZTAwNDBmMmZhJmNpZD1lZWYyNjdmNi0zZjUxLTVkNDYtYTcxNy03MTFhYzEyYmNmNzYiLCJzcnZjIjoieW91dHViZSIsInByb2R1Y3QiOiJzbWFydGxpbmsiLCJzaG9ydElkIjoiMjIxIiwiaXNBdXRob3JpemF0aW9uUmVxdWlyZWQiOmZhbHNlLCJvd25lciI6IjVlZTZiY2ViMzIwMDAwNjQyMmRkM2E2OCIsImFyIjoiNjE4MTJkYWYyNTAwMDBlNjc3YWZlN2YyIiwiaXNTaG9ydExpbmsiOmZhbHNlLCJuYXRpdmUiOmZhbHNlfQ',
     },
   },
   {
@@ -51,9 +51,9 @@ export const mockReleases: Release[] = [
 export const mockNews: NewsPost[] = [
   {
     id: '1',
-    title: 'HLPFL Records Announces New Artist Signing',
+    title: 'HLPFL Announces New Artist Signing',
     slug: 'new-artist-signing',
-    excerpt: 'We are thrilled to welcome our newest artist to the HLPFL Records family.',
+    excerpt: 'We are thrilled to welcome our newest artist to the HLPFL family.',
     content: 'Full article content placeholder...',
     publishedAt: new Date('2024-01-10'),
     author: 'HLPFL Team',
