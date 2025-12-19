81aea636804be9fc6adf12cd6943525e3346f36f
HLPFL <admin@hlpfl.co>
2025-12-19 17:19:02 -0500

    Update mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -232,7 +232,7 @@ export const mockTeam: TeamMember[] = [
     name: 'Alki',
     role: 'Co-Founder & Signed Artist',
     bio: 'Boundary-pushing artist creating the future of alternative music. As HLPFL\'s first signed artist and co-founder, Alki embodies our revolutionary artist-first philosophy. With a unique blend of punk, pop, and rap, Alki is proving that independent artists can thrive with the right partnership. Nearly 48,000 monthly listeners and millions of streams.',
-    image: '/images/team/alki.jpg'',
+    image: '/images/team/alki.jpg',
     socials: {
       instagram: 'https://instagram.com/alkiotis',
       spotify: 'https://open.spotify.com/artist/1Jof1vMpSF5pIWUvG9cizl',
